599dbac26aee8d296c97e33f56b88a1421e1d2b2
Kyrillus Khnifes <43353117+KhnifesKyrillus@users.noreply.github.com>
2019-04-25 00:25:41 +0200

    Update lottery.cpp

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -10,153 +10,107 @@
  * Implementation of a library for analyzing lottery tips.
  *-----------------------------------------------------------------------------
 */
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "lottery.h"
+#include "general.h"
+#include <string.h> // for strtok to split the string to substring with separator
+#include <stdlib.h> // for atoi to work (converts string to int)
+#include <stdio.h>
 
-#define 	UUID_LEN       36
-#define 	MAX_TIP_LEN    17
+#define 	UUID_LEN   40
+#define 	MAX_TIP_LEN   17
+#define 	MAX_LINE_LEN   (UUID_LEN + 1 + MAX_TIP_LEN + 1)
 
-#define 	MAX_LINE_LEN   (UUID_LEN + 1 + MAX_TIP_LEN)
-
-bool contains_tip(int* tip, int tipDigit);
-int get_number_of_lines(FILE* fd);
-
-FILE* fd;
-char separator;
-int last_drawing[TIP_SIZE];
+static FILE* stream;
+static char separator;
+static int drawing_of_numbers[TIP_SIZE];
 
 bool init_lottery(const char *csv_file, char csv_separator)
 {
-    fd = fopen(csv_file, "r");
-    separator = csv_separator;
-    for (int i = 0; i < TIP_SIZE; i++)
-        last_drawing[i] = 0;
-    return fd != 0;
+    
+  stream = fopen(csv_file, "r");
+  if (stream == 0) return false;
+  separator = csv_separator;
+    for (int i = 0; i < TIP_SIZE; ++i) {
+        drawing_of_numbers[i] = 0;
+    }
+  return true;
+}
+
+bool is_empty(int drawing_numbers[])
+{
+    for (int i = 0; i < TIP_SIZE; ++i) {
+        if (drawing_numbers[i] != 0) return false;
+    }
+    return true;
 }
 
 bool get_tip(int tip_number, int tip[TIP_SIZE])
 {
-    if (tip_number < 0 || tip_number >= 1000000)
-        return false;
-
-    fseek(fd, 0, SEEK_SET);
-    int currentLine = 0;
-    while (currentLine < tip_number)
+    if (tip_number < 0 || tip_number >= 44) return false;
+    int count = 0;
+    char line[MAX_LINE_LEN];
+    while ( fgets(line, MAX_LINE_LEN, stream) != NULL) // read a line
     {
-        while (!feof(fd) && fgetc(fd) == '\n') 
+        if (count == tip_number)
         {
-            currentLine++;
-            break;
+            count = 0;
+            char str[2];
+            str[0] = separator;
+            str[1] = '\0';
+            char* ptr = strtok(line, str); // gets the first string before separator
+            while(ptr != 0)
+            {
+                ptr = strtok(0, str); // gets next string
+                if (ptr == 0) break; // if 0 we reached EOL
+                if (atoi(ptr) > 0 && atoi(ptr) <= 45) // check if it is a valid tip number
+                {
+                    tip[count] = atoi(ptr); // converts the string to an int
+                }
+                count++;
+            }
+            rewind(stream); // sets current to 0 to start at the beginning next time
+            return true;
         }
+        count++;
     }
-
-    fseek(fd, (UUID_LEN + 1) * sizeof(char), SEEK_CUR);
-  
-    char tipString[MAX_TIP_LEN];
-    fgets(tipString, MAX_TIP_LEN, fd);
-
-    char delimiter[2] = { separator, '\0' };
-    char *ptr = strtok(tipString, delimiter);
-
-    int pos = 0;
-    while(ptr != NULL) 
-    {
-        int tipNumber = atoi(ptr);
-        tip[pos] = tipNumber;
-        ptr = strtok(NULL, delimiter);
-        pos++;
-    }
-    return true;
+  return false;
 }
 
 bool set_drawing(int drawing_numbers[TIP_SIZE])
 {
-    for (int i = 0; i < TIP_SIZE; i++)
-    {
-        if (drawing_numbers[i] <= 0 || drawing_numbers[i] > 45)
-            return false;
-    }
-
-    for (int i = 0; i < TIP_SIZE; i++)
-    {
-        last_drawing[i] = drawing_numbers[i];
-    }
-    return true;
+  for (int i = 0; i < TIP_SIZE; i++) {
+    if (drawing_numbers[i] <= 0 || drawing_numbers[i] > 45) return false;
+    else drawing_of_numbers[i] = drawing_numbers[i];
+  }
+  return true;
 }
 
-bool is_invalid_tip(int tip_number)
+int get_tip_result(int tip_number)
 {
-    int previousPos = ftell(fd);
-    fseek(fd, 0, SEEK_END);
-    int endPos = ftell(fd);
-    fseek(fd, previousPos, SEEK_SET);
-    return tip_number * MAX_LINE_LEN > endPos;
-}
-
-bool is_drawing_set()
-{
-    for (int i = 0; i < TIP_SIZE; i++)
-    {
-        if (last_drawing[i] == 0)
-            return false;
+  if (tip_number < 0 || tip_number >= 44) return -2;
+  if (is_empty(drawing_of_numbers))return -1;
+  int tip[TIP_SIZE];
+  int count = 0;
+  get_tip(tip_number,tip);
+  for (int i = 0; i < TIP_SIZE; i++) {
+    for (int j = 0; j < TIP_SIZE; j++) {
+      if (drawing_of_numbers[i] == tip[j]) count++;
     }
-    return true;
-}
-
-int get_tip_result(int tip_number) {
-    if (tip_number < 0 || tip_number >= get_number_of_lines(fd))
-        return -2;
-
-    if (!is_drawing_set())
-        return -1;
-
-    int csv_tip[TIP_SIZE];
-    get_tip(tip_number, csv_tip);
-
-    int correctDigits = 0;
-    for (int i = 0; i < TIP_SIZE; i++)
-    {
-        if (contains_tip(last_drawing, csv_tip[i]))
-            correctDigits++;
-    }
-    return correctDigits;
-}
-
-int get_number_of_lines(FILE* fd)
-{
-    int previousPos = ftell(fd);
-    fseek(fd, 0, SEEK_SET);
-    int lines = 1;
-    while (!feof(fd))
-    {
-        if (fgetc(fd) == '\n') lines++;
-    }
-    fseek(fd, previousPos, SEEK_SET);
-    return lines;
+  }
+  return count;
 }
 
 int get_right_tips_count(int right_digits_count)
 {
-    if (!is_drawing_set() || right_digits_count < 0 || right_digits_count > TIP_SIZE)
-        return -1;
-
+    if (right_digits_count < 0 || right_digits_count > TIP_SIZE || is_empty(drawing_of_numbers)) return -1;
     int count = 0;
-    int numOfLines = get_number_of_lines(fd);
-    for (int i = 0; i < numOfLines; i++)
-    {
-        int result = get_tip_result(i);
-        if (result == right_digits_count)
+    int temp = 0;
+    for (int i = 0; i < 44; ++i) {
+        temp = get_tip_result(i);
+        if (temp == right_digits_count)
+        {
             count++;
+        }
     }
-    return count;
-}
-
-bool contains_tip(int* tip, int tipDigit) {
-    for (unsigned int i = 0; i < TIP_SIZE; i++)
-    {
-        if (tip[i] == tipDigit) return true;
-    }
-    return false;
+  return count;
 }
